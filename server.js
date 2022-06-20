d1381e455fc1e141b1253ef6a06f0e87ec11b848
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-20 13:36:03 +0200

    ejs prueba

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ app.set('view engine', 'ejs');
 
 // Página index 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    let mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    let tagline = "Ningún concepto de programación está completo sin una linda mascota.";
+
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
 
 // Página about 
@@ -20,3 +30,12 @@ app.get('/about', function(req, res) {
 
 app.listen(8080);
 console.log('8080 este es el puerto mágico');
+
+
+/** Pasar datos a Vistas y Parciales.- Index.ejs
+Vamos a definir algunas variables básicas y una lista para pasar a nuestra página 
+de inicio. Vuelva a su archivo server.js y añada lo siguiente dentro de su 
+ruta app.get('/').
+Hemos creado una lista llamada mascots y una cadena sencilla llamada tagline. 
+Vamos a entrar en nuestro archivo index.ejs para usarlas.*/
+
